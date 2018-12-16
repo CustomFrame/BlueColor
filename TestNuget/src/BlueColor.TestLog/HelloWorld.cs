f2957943e7285c1329e87ec5348c5ec39fad15e2
智慧 梁 <214909012@qq.com>
2018-12-17 00:45:32 +0800

    添加测试

---
+++ TestNuget/src/BlueColor.TestLog/HelloWorld.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BlueColor.TestLog
+{
+    /// <summary>
+    /// HelloWorld
+    /// </summary>
+    public class HelloWorld
+    {
+        /// <summary>
+        /// 问候
+        /// </summary>
+        /// <param name="nickName">昵称</param>
+        public static string SayHello(string nickName)
+        {
+            return $"Hello,{nickName}!";
+        }
+    }
+}
---
+++ TestNuget/tests/BlueColor.TestLog.Tests/HelloWorldTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BlueColor.TestLog.Tests
+{
+    [TestClass]
+    public class HelloWorldTest
+    {
+        [TestMethod]
+        public void SayHelloTest()
+        {
+            string reuslt = HelloWorld.SayHello("BlueLover");
+
+            Assert.AreEqual(reuslt, "Hello,BlueLover!");
+        }
+    }
+}
