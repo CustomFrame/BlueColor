0b945bd7d2112b3bdfd08f19156f006f3eeb0366
智慧 梁 <214909012@qq.com>
2019-01-11 21:20:54 +0800

    Add "BcLog.GetLogger" Static Method（返回日志记录器-指定日志文件名）

--- TestNuget/docs/ReadMe.md
+++ TestNuget/docs/ReadMe.md
@@ -1,4 +1,6 @@
-﻿using System;
+eg:
+```C#
+using System;
 
 namespace ConsoleApp1
 {
@@ -14,3 +16,12 @@ namespace ConsoleApp1
         }
     }
 }
+```
+Console
+```text 
+你好,世界!
+```
+HelloLogFile.Info.log
+```text
+2019-01-11 21:05:34.0027|INFO|HelloLogFile|你好,世界!
+```
---
+++ TestNuget/src/BlueColor.TestLog/BcLog.cs
@@ -0,0 +1,33 @@
+﻿using NLog;
+using NLog.Targets;
+using System.Text;
+
+namespace BlueColor.TestLog
+{
+    /// <summary>
+    /// Bc日志
+    /// </summary>
+    public class BcLog
+    {
+        /// <summary>
+        /// 返回日志记录器
+        /// </summary>
+        /// <param name="logFileName">日志文件名</param>
+        /// <returns></returns>
+        public static Logger GetLogger(string logFileName)
+        {
+            var target = new FileTarget();
+
+            target.FileName = "${basedir}/logs/${shortdate}/" + logFileName + ".${level}.log";
+            target.OpenFileCacheTimeout = 30;
+            target.KeepFileOpen = true;
+            target.Encoding = Encoding.UTF8;
+
+            NLog.Config.SimpleConfigurator.ConfigureForTargetLogging(target, LogLevel.Trace);
+
+            Logger logger = LogManager.GetLogger(logFileName);
+
+            return logger;
+        }
+    }
+}
