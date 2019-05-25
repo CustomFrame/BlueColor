9bec516c3a0a7f2602a8c887423edc7fc363677e
智慧 梁 <214909012@qq.com>
2019-05-25 09:39:01 +0800

    控制器

---
+++ BlueColor.VueTemplate/BlueColor.Vue.Mvc/Controllers/VueDataController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlueColor.Vue.Mvc.Controllers
+{
+    public class VueDataController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BlueColor.VueTemplate/BlueColor.Vue.Mvc/Interface/IBcVueData.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlueColor.Vue.Mvc.Interface
+{
+    /// <summary>
+    /// IBcVueData
+    /// </summary>
+    interface IBcVueData
+    {
+        IActionResult Index();
+
+        IActionResult HandleSearch();
+
+        IActionResult HandleSave();
+
+        IActionResult HandleDelete();
+    }
+}
