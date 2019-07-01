0fa3c67f7e8d0b2b25cb1fa7f6c8400f9dc08ad0
智慧 梁 <214909012@qq.com>
2019-07-01 14:59:07 +0800

    归档

---
+++ BlueColor.VueTemplate/BlueColor.Vue.Mvc/AAA/Hello1.cs
@@ -0,0 +1,32 @@
+﻿namespace BlueColor.Vue.Mvc.AAA
+{
+    /// <summary>
+    /// hello1
+    /// </summary>
+    public class Hello1
+    {
+        private string mHello = string.Empty;
+
+        /// <summary>
+        ///
+        /// </summary>
+        public Hello1()
+        {
+            mHello = "Hello";
+        }
+
+
+
+        #region MyRegion
+
+        //! asdfasdf sdfds
+        //? sadfasdfsdf
+        //x hahahahadshfsadhf
+        //!? wohsadof
+        //TODO: 哇哈哈哈
+
+        #endregion MyRegion
+
+        //? Passion knew holy mammon her feeble high pleasure delphis wandered loved to and from heralds lands himnot their and soon
+    }
+}
---
+++ BlueColor.VueTemplate/BlueColor.Vue.Mvc/AAA/JavaScript.js
@@ -0,0 +1,8 @@
+﻿foo(arg1, arg2, arg3, arg4);
+foo(
+  reallyLongArg(),
+
+  omgSoManyParameters(),
+  IShouldRefactorThis(),
+  isThereSeriouslyAnotherOne()
+);
